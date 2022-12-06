9ad6bf317cdac14cda7ba8a084cda4ec2572a9f9
Moosa <Moosa>
2022-12-06 16:06:02 +0500

    ok done again

--- webserver.py
+++ webserver.py
@@ -20,6 +20,7 @@ app = Flask(__name__)
 
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
 app.secret_key = "super secret key"
 app.url_map.strict_slashes = False
